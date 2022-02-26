3bbb54190d38d7bb63cbec43e9a1ba38b3a7857d
Michalis Pardalos <m.pardalos@gmail.com>
2022-02-26 13:17:31 +0000

    Let basic_to_bin.py take input from stdin

--- data/basic_to_bin.py
+++ data/basic_to_bin.py
@@ -1,7 +1,15 @@
+#!/bin/env python3
 import sys
 
-infile = open(sys.argv[1], 'rt')
-outfile = open(sys.argv[2], 'wb')
+if len(sys.argv) == 2:
+    infile = sys.stdin
+    outfile = open(sys.argv[1], 'wb')
+elif len(sys.argv) == 3:
+    infile = open(sys.argv[1], 'rt')
+    outfile = open(sys.argv[2], 'wb')
+else:
+    print("You must provide at least one argument")
+    exit(1)
 
 for char in infile.read():
     # little/big is irrelevant since it's 1 byte
